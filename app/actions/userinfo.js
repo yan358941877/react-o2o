7998ef2b48bc0a632217011e5f964c85c4c738c6
yan358941877 <yan358941877@163.com>
2017-05-19 00:29:04 +0800

    bindActionCreators

--- app/actions/userinfo.js
+++ app/actions/userinfo.js
@@ -1,5 +1,6 @@
 import * as actionTypes from '../constants/userinfo'
 
+// ActionCreator 动作产生器--->向指定的方法传递数据，生成对应的action
 export function update(data){
     return {
         type: actionTypes.USERINFO_UPDATE,
